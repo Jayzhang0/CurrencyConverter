e4d3c318bfcf87fb500d9e94ba1d622f3ae6d8ea
Jayzhang0 <shujie2@illinois.edu>
2020-04-26 15:58:36 -0400

    h

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    skdnakjbwhkbksda
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
