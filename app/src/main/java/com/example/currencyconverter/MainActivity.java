ba85097878f73befa34af74f1d6e70aac29c6422
edwinlu <jiajun3@illinois.edu>
2020-04-26 15:07:00 -0500

    bkihbik

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     skdnakjbwhkbksda
-    kggikjnibhj
+    kggikjnibhjigygi
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
