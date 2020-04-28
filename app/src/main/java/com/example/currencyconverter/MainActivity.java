02ee81cb664e2bd29b22379ec9b9a5f82f45ae9d
edwinlu <jiajun3@illinois.edu>
2020-04-28 04:48:04 -0500

    Something

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    skdnakjbwhkbksda
-    kggikjnibhjigygihjkjh
-
+    EditText input, output;
+    Button button;
+    String from, to;
+    void setupListeners() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        input = findViewById(R.id.from);
+        output = findViewById(R.id.to);
+        button = findViewById(R.id.button);
     }
 }
